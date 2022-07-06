bf2326db1dbbde262fc00a484fdbf9de9320cf9e
Upadhi Vijay <35913836+UpadhiV@users.noreply.github.com>
2022-07-05 21:34:55 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -58,24 +58,42 @@ def graph_plot( bus_cost, utility, fuel_price, dc_efficiency, demand_charge, bus
    #estimating number of chargers 
     num_chargers  = np.ceil((energy_data["energy_"+ bus_type]/7)/(calc_lib["charge_power_dc"].values[0]* bus_lib[bus_lib.type == bus_type]["charging_time"].values[0]*dc_efficiency))
     
-    #capital costs
-    capital_cost_e = (bus_cost+ cost_lib["batt_cost27"].values[0]* bus_lib[bus_lib.type == bus_type]["battery_size"].values[0]) *  energy_data.num_buses + num_chargers*(cost_lib["charger_costs"][0]+cost_lib["instal_costs"][0])
+   #capital costs
+    capital_cost_e = (bus_cost) *  energy_data.num_buses + num_chargers*(cost_lib["charger_costs"][0]+cost_lib["instal_costs"][0])
     capital_cost_d = cost_lib[bus_type+"_cost"][0] * energy_data.num_buses 
     
     #Vehicle maintainance costs 
-    vmaint_cost_e = cost_lib["vehicle_main_e"][0]* energy_data.VKT * 0.621371*52*12 #coverting km to miles 
-    vmaint_cost_d = cost_lib["vehicle_ main_d"][0]* energy_data.VKT * 0.621371*52*12
+    vmaint_cost_e = cost_lib["vehicle_main_e"][0]* energy_data.VKT * 0.621371*52 #coverting km to miles 
+    vmaint_cost_d = cost_lib["vehicle_ main_d"][0]* energy_data.VKT * 0.621371*52
     
     #charging/fueling costs 
-    fuel_costs_e = ((energy_data["energy_" + bus_type]*4.35*utility)/dc_efficiency )*12*(11.93422) + (num_chargers*calc_lib["charge_power_dc"][0]* demand_charge)*12*12
-    fuel_costs_d = fuel_price * (energy_data.VKT * 0.621371 /energy_data["fuel_economy"])*52*12.47295
+    fuel_costs_e = ((((energy_data["energy_" + bus_type]*4.35*utility)/dc_efficiency )*12*((1+calc_lib["elec_rate"][0])**12 -1)/((1+calc_lib["elec_rate"][0])-1)) + ((num_chargers*calc_lib["charge_power_dc"][0]* demand_charge)*12*(calc_lib["annuity_factor"].values[0]-1)/(calc_lib["discount_rate"].values[0]*calc_lib["annuity_factor"].values[0])))#(11.93422)
+    fuel_costs_d = (fuel_price * (energy_data.VKT * 0.621371 /energy_data["fuel_economy"])*52)*(((1+calc_lib["fuel_rate"][0])**12 -1)/((1+calc_lib["fuel_rate"][0])-1))
     
     #infrastructure operations and maintainance cost 
-    infra_onm_e = cost_lib["charging _onm"][0] * num_chargers *12
-    infra_onm_d = cost_lib["fuel_infra_costs"][0] * (energy_data.VKT * 0.621371 /energy_data["fuel_economy"])*52*12
+    infra_onm_e = cost_lib["charging _onm"][0] * num_chargers 
+    infra_onm_d = cost_lib["fuel_infra_costs"][0] * (energy_data.VKT * 0.621371 /energy_data["fuel_economy"])*52
 
-    cities["TCO per route - D"]  =  np.round((capital_cost_d+vmaint_cost_d+fuel_costs_d+infra_onm_d)/calc_lib["annuity_factor"].values[0] )
-    cities["TCO per route - E"]  = np.round((capital_cost_e+vmaint_cost_e+fuel_costs_e+infra_onm_e)/calc_lib["annuity_factor"].values[0] )
+    #salvage costs 
+    sal_e = (bus_cost)* calc_lib["bus_salvage"][0]*energy_data.num_buses +cost_lib["charger_costs"][0]* calc_lib["char_salvage"][0]*num_chargers
+    sal_d = cost_lib[bus_type+"_cost"][0]*calc_lib["bus_salvage"][0]*energy_data.num_buses
+
+    capital_d  = capital_cost_d/1000000
+    capital_e = capital_cost_e/1000000
+    vmain_e = (vmaint_cost_e *(calc_lib["annuity_factor"].values[0]-1))/(calc_lib["discount_rate"].values[0]*calc_lib["annuity_factor"].values[0]*1000000)
+    fuel_e = (fuel_costs_e/1000000)
+    fuel_d = (fuel_costs_d/1000000)
+    onm_d =  (infra_onm_d*(calc_lib["annuity_factor"].values[0]-1))/(calc_lib["discount_rate"].values[0]*calc_lib["annuity_factor"].values[0]*1000000)
+    vmain_d = (vmaint_cost_d*(calc_lib["annuity_factor"].values[0]-1))/(calc_lib["discount_rate"].values[0]*calc_lib["annuity_factor"].values[0]*1000000)
+    onm_e = (infra_onm_e*(calc_lib["annuity_factor"].values[0]-1))/(calc_lib["discount_rate"].values[0]*calc_lib["annuity_factor"].values[0]*1000000)
+    sal_e = -sal_e/(calc_lib["annuity_factor"].values[0]*1000000)
+    sal_d = -sal_d/(calc_lib["annuity_factor"].values[0]*1000000)
+    tco_d = capital_d+vmain_d+fuel_d+onm_d+sal_d
+    tco_e = capital_e+vmain_e+fuel_e+onm_e+sal_e
+
+    
+    cities["TCO per route - D"]  =  np.round(tco_d)
+    cities["TCO per route - E"]  = np.round(tco_e)
     cities["No of buses"] = energy_data.num_buses
     cities["Emissions -E"] = np.round(energy_data["e_emi"+bus_type], decimals = 2)
     cities["Emissions -D"] = np.round(energy_data.d_emi, decimals = 2)
